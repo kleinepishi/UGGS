d22284435c849fc6a0f07864bbbeab65aa7401fc
Nathaniel Maki <nmaki@amunet.local>
2019-09-05 11:27:00 -0400

    update UGGS with wormmine fetcher

--- UGGS_mk1.py
+++ UGGS_mk1.py
@@ -6,23 +6,59 @@
 import pandas as pd
 from pandas import ExcelFile
 from pandas import ExcelWriter
+from intermine.webservice import Service
 import numpy as np
 
-# excel file
+# wormmine parameter query
+def wmquery():
+    service = Service("http://intermine.wormbase.org/tools/wormmine/service")
+    query = service.new_query("Gene")
+    query.add_view(
+        "biotype", "length", "symbol", "primaryIdentifier",
+        "downstreamIntergenicRegion.primaryIdentifier",
+        "downstreamIntergenicRegion.organism.name",
+        "downstreamIntergenicRegion.locations.feature.primaryIdentifier",
+        "downstreamIntergenicRegion.locations.start",
+        "downstreamIntergenicRegion.locations.end",
+        "downstreamIntergenicRegion.locations.strand", "homologues.dataSets.name",
+        "upstreamIntergenicRegion.primaryIdentifier",
+        "upstreamIntergenicRegion.organism.name",
+        "upstreamIntergenicRegion.locations.feature.primaryIdentifier",
+        "upstreamIntergenicRegion.locations.start",
+        "upstreamIntergenicRegion.locations.end",
+        "upstreamIntergenicRegion.locations.strand",
+        "transcripts.primaryIdentifier", "transcripts.symbol"
+    )
 
-data_file = pd.read_csv('jesse_data.csv')
+    for row in query.rows():
+        print (row["biotype"], row["length"], row["symbol"], row["primaryIdentifier"], \
+            row["downstreamIntergenicRegion.primaryIdentifier"], \
+            row["downstreamIntergenicRegion.organism.name"], \
+            row["downstreamIntergenicRegion.locations.feature.primaryIdentifier"], \
+            row["downstreamIntergenicRegion.locations.start"], \
+            row["downstreamIntergenicRegion.locations.end"], \
+            row["downstreamIntergenicRegion.locations.strand"], row["homologues.dataSets.name"], \
+            row["upstreamIntergenicRegion.primaryIdentifier"], \
+            row["upstreamIntergenicRegion.organism.name"], \
+            row["upstreamIntergenicRegion.locations.feature.primaryIdentifier"], \
+            row["upstreamIntergenicRegion.locations.start"], \
+            row["upstreamIntergenicRegion.locations.end"], \
+            row["upstreamIntergenicRegion.locations.strand"], row["transcripts.primaryIdentifier"], \
+            row["transcripts.symbol"])
+wmquery()
 
-def load(data_file):
+# define primary data file
+df = pd.read_excel('jesse_data.xlsx')
+
+def load(df):
     # load in dataframe
-    df = data_file
     # display column headings
     print("Column headings:")
     print(df.columns)
-load(data_file)
+load(df)
 
 # print out all genes where value is > 0
 def threshold():
-    df = data_file
     thresh_df = df[['gene_id',
         '531 genes where translation is promoted by GLH-1 (-.2 difference, see old table for details)',
         '587 genes where translation is inhibited by GLH-1 (.2 difference, see old table for details)',
@@ -33,10 +69,4 @@ def threshold():
         (thresh_df['587 genes where translation is inhibited by GLH-1 (.2 difference, see old table for details)'] > 0) &
         (thresh_df['97 genes where translation is promoted by GLH-1 (-.5 difference, see old table for details)'] > 0) &
         (thresh_df['99 genes where translation is inhibited by GLH-1 (.5 difference, see old table for details)'] > 0)]
-    print(thresh_val()
-
-#def seg(data_file):
-    # load in dataframe
- #   df = pd.read_excel(data_file, index_col=None, na_values=['NA'], usecols="A,C,D,E,F")
-  #  print(df.columns)
-#seg(data_file)
+    print(thresh_val())
