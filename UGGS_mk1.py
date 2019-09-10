d59f38309dcbc91937e79ae22a1e58da3729ef89
Nathaniel Maki <nmaki@amunet.local>
2019-09-10 13:18:22 -0400

    update UGGS with new functions

--- UGGS_mk1.py
+++ UGGS_mk1.py
@@ -72,3 +72,36 @@ def threshold():
         (thresh_df['99 genes where translation is inhibited by GLH-1 (.5 difference, see old table for details)'] > 0)]
     print(thresh_val())
 
+#pd.options.display.width = 0
+# load in dataset to use for analysis key
+# "selection" gene file for c.elegans
+# create dataframe
+def ana_threshold():
+    df = pd.read_csv('jesse_data.csv', index_col=0)
+    ana_df = pd.DataFrame(df)
+    # select only columns 1,2,3,4,5
+    gene_groups = ana_df.iloc[:, [0,1,2,3]]
+    print(gene_groups)
+
+    #gene_thresh = gene_groups.iloc[(gene_groups[:, [2,3,4,5] >=0 ])]
+    #print(gene_thresh)
+    #thresh_group_531 = group_531[group_531.iloc[:, [0]] >= 1]
+    #print(thresh_group_531)
+
+    for column, row in ana_df.iterrows():
+        print(row)
+
+ana_threshold()
+
+# load in dataset to use for analysis selection
+# "master" gene file for c. elegans
+# create dataframe
+def master_dict():
+    df = pd.read_csv('c_elegans_gene_data.csv')
+    ref_df = pd.DataFrame(df)
+    # use [:,3] to load column 3 of the dataframe
+    ref_gene_id = ref_df.iloc[:,3]
+    #print(ref_gene_id)
+    #print(ref_df)
+master_dict()
+
